53a7b80e96e8b988b7154f52ed71332485f16134
Anuj Joshi <101502385+1AnujJoshi@users.noreply.github.com>
2023-01-31 12:44:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./components/App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3008/";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
